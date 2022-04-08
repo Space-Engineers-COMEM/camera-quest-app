4a0551af6be73b9a2d2aad45d5d4c7ff492a40c0
Robin Zweifel <robin.zweifel@gmail.com>
2022-04-08 15:56:13 +0200

    Add basic front UI translation features

--- src/input/LanguageSelector.tsx
+++ src/input/LanguageSelector.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation, Trans } from 'react-i18next';
 
 interface Props {
   // onChange: React.MouseEventHandler<HTMLButtonElement>;
@@ -7,36 +8,46 @@ interface Props {
   defaultLang: string;
 }
 
-export default class LanguageSelector extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
+export default function LanguageSelector(props: Props) {
+  const lngs = {
+    fr: { nativeName: 'Français' },
+    en: { nativeName: 'English' },
+  };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const { t, i18n } = useTranslation();
 
   // Handeled internaly
-  handleChange(evt: any) {
-    this.props.onChange(evt.currentTarget.value);
-  }
+  const handleChange = (evt: any) => {
+    props.onChange(evt.currentTarget.value);
+  };
 
   // Handeled by parent
-  handleSubmit(evt: any) {
-    this.props.onSubmit(evt);
-  }
+  const handleSubmit = (evt: any) => {
+    props.onSubmit(evt);
+  };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>Change the language: </p>
-        <select value={this.props.defaultLang} onChange={this.handleChange}>
-          <option value="fr">Français</option>
-          <option value="de">Deutsch</option>
-          <option value="it">Italiano</option>
-          <option value="en">English</option>
-        </select>
-        <input type="submit" value="X" />
-      </form>
-    );
-  }
+  const handleIt = (evt: any) => {
+    evt.preventDefault();
+    i18n.changeLanguage(evt.target.value);
+    handleSubmit(evt);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>Change the language: </p>
+      <select value={i18n.language} onChange={handleIt}>
+        {Object.keys(lngs).map((lng) => (
+          <option
+            key={lng}
+            style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }}
+            onSelect={() => i18n.changeLanguage(lng)}
+            value={lng}
+          >
+            {lngs[lng as keyof typeof lngs].nativeName}
+          </option>
+        ))}
+      </select>
+      <input type="submit" value="X" />
+    </form>
+  );
 }
