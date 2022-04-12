3cadbeccf9b9d58c1dd65d9decad7a1c97e146db
Mikael Ruffieux <ruffieux.mikael@gmail.com>
2022-04-12 15:01:17 +0200

    Merging

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import 'intro.js/introjs.css';
-import './index.css';
-=======
 import './index.scss';
->>>>>>> main
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
