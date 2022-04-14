128a07291fe330a463c73737581c64bb1a2ee559
Kevin Oliveira Paiva <56036350+Kevoliv@users.noreply.github.com>
2022-04-14 13:24:41 +0200

    Add bootstrap

--- src/POI.tsx
+++ src/POI.tsx
@@ -52,36 +52,59 @@ export default function POI() {
       <img
         className="poi_img"
         // src="https://www.publicdomainpictures.net/pictures/30000/nahled/old-camera-1352392502n6P.jpg"
-
         src="/4131_MEGALETOSCOPE.jpg"
         alt="Vieil appareil photographique"
       />
-      <h1 className="poi_title">{POIToShow.title} </h1>
-      <div className="checkbox">
-        <input type="checkbox" checked /* checked= {POIToShow.visible ? 'checked' : ''} */ />
-        <span className="checkmark" />
+      <div className="container-sm m-0 p-0">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="poi_title">{POIToShow.title} </h1>
+            <div className="checkbox">
+              <input type="checkbox" checked /* checked= {POIToShow.visible ? 'checked' : ''} */ />
+              <span className="checkmark" />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <h2 className="poi_author">{POIToShow.author}</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-auto">
+            <h3 className="poi_detail">
+              {POIToShow.periode}
+              {canBeShared ? (
+                <span>
+                  {' '}
+                  |{' '}
+                  <button type="button" onClick={handleShareClick}>
+                    <i className="fa-solid fa-share-from-square" />
+                  </button>
+                </span>
+              ) : (
+                ''
+              )}
+            </h3>
+          </div>
+          <div className="col-md-auto">
+            <p className="poi_detail"> | </p>
+          </div>
+          <div className="col-md-auto">
+            <h3 className="poi_detail">{POIToShow.origin}</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <p className="poi_description">{POIToShow.description}</p>
+          </div>
+        </div>
       </div>
-      <h2 className="poi_author">{POIToShow.author}</h2>
-      <h3 className="poi_detail">
-        {POIToShow.periode}
-        {canBeShared ? (
-          <span>
-            {' '}
-            |{' '}
-            <button type="button" onClick={handleShareClick}>
-              <i className="fa-solid fa-share-from-square" />
-            </button>
-          </span>
-        ) : (
-          ''
-        )}
-      </h3>
-      <p className="poi_detail"> | </p>
-      <h3 className="poi_detail">{POIToShow.origin}</h3>
-      <p className="poi_description">{POIToShow.description}</p>
-      <AudioPlayer src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
-      {/* <div>Object {id}</div>
-      <p>Hello</p> */}
+      <div>
+        <AudioPlayer src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
+        {/* <div>Object {id}</div>
+    <p>Hello</p> */}
+      </div>
     </div>
   );
 }
