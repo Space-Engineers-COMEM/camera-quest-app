a1318f18f289d0194c3c9a0531fb77f9bf86add7
Kevin Oliveira Paiva <56036350+Kevoliv@users.noreply.github.com>
2022-04-21 18:12:37 +0200

    Fix Tick checkbox style

--- src/content/PoiCheck.tsx
+++ src/content/PoiCheck.tsx
@@ -8,7 +8,22 @@ export default function PoiCheck(props: Props) {
   const check = props.checked ? (
     <div className="checkbox">
       <input type="checkbox" checked readOnly /* checked= {POIToShow.visible ? 'checked' : ''} */ />
-      <span className="checkmark" />
+      <svg
+        className="checkmark"
+        width="6"
+        height="6"
+        viewBox="-1.5 0 9 5"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.00049 2.89168L2.60036 4.69154L5.00016 0.891846"
+          stroke="white"
+          strokeWidth="0.959922"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     </div>
   ) : (
     <div className="checkbox">
