0f0ae3d532994c06226fe30bd0f3456c07cd3e7e
Luka Najjar <lukanajjar@hotmail.com>
2022-04-21 20:59:20 +0200

    Update image processing CSS

--- src/content/PoiPreview.tsx
+++ src/content/PoiPreview.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useNavigate } from 'react-router-dom';
+import Button from '../navigation/Button';
 import PoiPreviewType from '../types/PoiPreviewType';
+import PoiCheck from './PoiCheck';
 
 interface Props {
   poi?: PoiPreviewType;
+  onCloseFeedback: any;
 }
 
 export default function PoiPreview(props: Props) {
   const { t } = useTranslation('', { keyPrefix: 'Snap' });
   const { poi } = props;
+
+  const onCloseFeedback = (): void => {
+    props.onCloseFeedback();
+  };
+
+  const getCapturedPOIs = (): number[] => {
+    const stringPOIs = localStorage.getItem('captured-pois');
+    return stringPOIs ? JSON.parse(stringPOIs) : [];
+  };
+
+  const isCapturedPOI = (id: number): boolean =>
+    typeof getCapturedPOIs().find((el) => el === id) !== 'undefined';
+
   if (poi) {
     return (
-      <div className="preview preview-poi">
-        {/* <img src={poi.imagePath} alt={poi.title} /> */}
-        <h3>{poi.title}</h3>
-        <Link className="btn open-btn" to={`/poi/${poi.id}`}>
+      <div className="cameraPreview">
+        <div>
+          <Button class="cameraCloseButton" onClick={onCloseFeedback}>
+            <span>X</span>
+          </Button>
+          <img src={poi.imagePath} alt={poi.title} />
+          <div>
+            <h3>{poi.title}</h3>
+            <PoiCheck checked={isCapturedPOI(poi.id)} />
+          </div>
+        </div>
+        <Link className="btn" to={`/poi/${poi.id}`}>
           {t('open')}
         </Link>
       </div>
